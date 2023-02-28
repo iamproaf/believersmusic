923a5b7eb6562899c564430031e8ebd208131f6c
iamproaf <88786146+iamproaf@users.noreply.github.com>
2023-02-28 19:22:18 +0530

    Update play.py

--- AnonX/plugins/play.py
+++ AnonX/plugins/play.py
@@ -49,7 +49,7 @@ async def play_commnd(
     url,
     fplay,
 ):
-if not await is_served_user(message.from_user.id):
+    if not await is_served_user(message.from_user.id):
         await message.reply_text(
             text="ᴇʀʀᴏʀ, ʏᴏᴜ'ʀᴇ ɴᴏᴛ ᴀ ᴠᴇʀɪғɪᴇᴅ ᴜsᴇʀ.\nᴘʟᴇᴀsᴇ ᴄʟɪᴄᴋ ᴏɴ ᴛʜᴇ ʙᴇʟᴏᴡ ʙᴜᴛᴛᴏɴ ᴛᴏ ᴠᴇʀɪғʏ ʏᴏᴜʀsᴇʟғ.",
             reply_markup=InlineKeyboardMarkup(
